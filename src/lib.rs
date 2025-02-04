eb5705b1e29be3b9bc7141ca9c93672882be7b92
Paraworker <paraworker@gmail.com>
2025-02-04 21:37:27 +0000

    Optimize drop logic

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use crossbeam_channel::{unbounded, Receiver, Sender};
 use log::info;
 use std::{
+    mem::ManuallyDrop,
     num::NonZeroUsize,
     thread::{self, JoinHandle},
 };
@@ -8,11 +9,7 @@ use std::{
 type Task = Box<dyn FnOnce() + Send>;
 
 pub struct ThreadPool {
-    /// Order matters here
-    /// Drop sender first to stop workers
-    sender: Sender<Task>,
-
-    #[allow(dead_code)]
+    sender: ManuallyDrop<Sender<Task>>,
     workers: Vec<Worker>,
 }
 
@@ -25,7 +22,10 @@ impl ThreadPool {
             .map(|id| Worker::spawn(id, receiver.clone()))
             .collect();
 
-        Self { sender, workers }
+        Self {
+            sender: ManuallyDrop::new(sender),
+            workers,
+        }
     }
 
     /// Execute a task
@@ -37,15 +37,32 @@ impl ThreadPool {
     }
 }
 
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        // Drop sender first so that workers can terminate
+        // SAFETY: sender will not be used anymore
+        unsafe { ManuallyDrop::drop(&mut self.sender) };
+
+        // Wait for all workers to finish
+        for worker in self.workers.drain(..) {
+            worker.join();
+        }
+    }
+}
+
 struct Worker {
-    thread: Option<JoinHandle<()>>,
+    thread: JoinHandle<()>,
 }
 
 impl Worker {
     fn spawn(id: usize, receiver: Receiver<Task>) -> Self {
         Self {
-            thread: Some(thread::spawn(move || Self::task_loop(id, receiver))),
+            thread: thread::spawn(move || Self::task_loop(id, receiver)),
         }
+    }
+
+    fn join(self) {
+        self.thread.join().unwrap();
     }
 
     fn task_loop(id: usize, receiver: Receiver<Task>) {
@@ -54,12 +71,6 @@ impl Worker {
             task();
         }
 
-        info!("Worker[{id}] terminate!");
-    }
-}
-
-impl Drop for Worker {
-    fn drop(&mut self) {
-        self.thread.take().unwrap().join().unwrap();
+        info!("Worker[{id}] terminated!");
     }
 }
